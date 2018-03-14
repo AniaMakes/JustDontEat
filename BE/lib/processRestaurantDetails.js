9407d85feb44e412c08cca4f18e445c924fee34f
CskCnklc <cskcnklc@gmail.com>
2018-03-14 16:57:38 +0000

    add sorting to reviews in details

--- BE/lib/processRestaurantDetails.js
+++ BE/lib/processRestaurantDetails.js
@@ -6,7 +6,7 @@ function processRestaurantDetails(details){
   outputRestaurantDetailsObject.address = details.formatted_address;
   outputRestaurantDetailsObject.rating = details.rating;
   outputRestaurantDetailsObject.contactDetails = details.international_phone_number;
-  outputRestaurantDetailsObject.reviews = details.reviews;
+  outputRestaurantDetailsObject.reviews = details.reviews.sort((a,b)=> a.rating-b.rating);
 
   return outputRestaurantDetailsObject;
 }
