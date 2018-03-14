c579b7bbbc7ffcc8c75881562ca15b7c0a8b25dd
CskCnklc <cskcnklc@gmail.com>
2018-03-14 14:52:47 +0000

    update api.js

--- BE/api.js
+++ BE/api.js
@@ -1,42 +1,75 @@
-const {googlePlacesApiKey ,googlePlacesApiURL} = process.env
+const { googlePlacesApiKey, googlePlacesApiURL } = process.env
 
 const fetch = require('node-fetch');
 //upate functions to get parameters from queries and send json back also catch errors
 
-function getPlaces(req,res){
-    // const keyword,lat,long, radius=1000
-const url= `${googlePlacesApiURL}/nearbysearch/json?location=${lat},${long}&radius=${radius}&type=restaurant&keyword=${keyword}&key=${googlePlacesApiKey}`;
-console.log(url); 
-return fetch(url)
+
+// try url http://localhost:3000/api/get-places?keyword=chinese&lat=-33.8670522&long=151.1957362
+function getPlaces(req, res) {
+    // getting queries from request
+    const keyword = req.query.keyword;
+    const lat = req.query.lat;
+    const long = req.query.long;
+    // assigning default search radius if not specified
+    const radius = req.query.radius || 10000;
+    //creating an empty array to fill
+    fillArr=[]
+    //defining how many results we would like to fetch minimum
+    minLength=50;
+    
+    //compiling fetch url
+    const url = `${googlePlacesApiURL}/nearbysearch/json?location=${lat},${long}&radius=${radius}&type=restaurant&keyword=${keyword}&key=${googlePlacesApiKey}`;
+    fetch(url).then((response) => {
+        return response.json()
+    }).then(data => {
+        fillArr=data.results
+        if(data.next_page_token) getNextPage(data.next_page_token,fillArr,minLength,res) 
+    }).catch(err => { 
+        console.log(err)  
+    })
 }
 
+function getNextPage(nextPageToken,fillArr,minLength,res) {
+    const url = `${googlePlacesApiURL}/nearbysearch/json?pagetoken=${nextPageToken}&key=${googlePlacesApiKey}`;
+    setTimeout(()=>{
+        fetch(url).then((response) => {
+            return response.json()
+        }).then(data=>{
+            fillArr=fillArr.concat(data.results);
+            if(data.next_page_token && fillArr.length<minLength) {
+                getNextPage(data.next_page_token,fillArr,minLength,res)
+            }else{
+                //Ania's sorting and striping function should go here before send.
+                res.status(200).json({results:fillArr});
+            } 
+        }).catch(err=> console.log(err))
+    },2000)
+   
+}
 
-function getPlaceDetails(req,res){
+function getPlaceDetails(req, res) {
     const placeId = req.query.placeId
-    console.log(placeId);
-    const url= `${googlePlacesApiURL}/details/json?placeid=${placeId}&key=${googlePlacesApiKey}`;
-    console.log(url)
-    fetch(`${googlePlacesApiURL}/details/json?placeid=${placeId}&key=${googlePlacesApiKey}`).then((response)=>{
+    const url = `${googlePlacesApiURL}/details/json?placeid=${placeId}&key=${googlePlacesApiKey}`;
+    fetch(url).then((response) => {
         return response.json()
-    }).then(data=>{
+    }).then(data => {
+        //details stripping function should go here
         res.status(200).json(data)
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err)
     })
 }
 
-function getNextPage(nextPageToken){
-    const url= `${googlePlacesApiURL}/nearbysearch/json?pagetoken=${nextPageToken}&key=${googlePlacesApiKey}`;
-}
 
-function getPhoto(id){
+
+function getPhoto(id) {
 
 }
 
-function getGeolocation(searchTerm){
-    
+function getGeolocation(searchTerm) {
+
 }
 
 
 
-module.exports = {getPlaces,getPlaceDetails};
+module.exports = { getPlaces, getPlaceDetails };
