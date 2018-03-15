9c04a9aa2f15c52978669254c7872e90a2b69a97
CskCnklc <cskcnklc@gmail.com>
2018-03-15 14:35:30 +0000

    api update and search trimming for based on rating

--- BE/api.js
+++ BE/api.js
@@ -4,32 +4,31 @@ const googlePlacesApiURL='https://maps.googleapis.com/maps/api/place'
 const processRestaurantSearch = require('./lib/processRestaurantSearch');
 const processRestaurantDetails = require('./lib/processRestaurantDetails');
 const fetch = require('node-fetch');
-
+let counter =0;
 function getPlaces(req, res) {
+    counter = 0;
     // getting queries from request
-    const keyword = req.query.keyword? '&keyword='+req.query.keyword : ''
-    
+    const keyword = req.query.keyword? req.query.keyword : 'food'
+
     const lat = req.query.lat;
     const long = req.query.long;
     // assigning default search radius if not specified
-    const radius = req.query.radius || 10000;
+    const radius = req.query.radius || 2000;
     //creating an empty array to fill
-    fillArr=[]
+    let fillArr=[]
     //defining how many results we would like to fetch minimum
-    minLength=10;
+    let minLength=10;
     
     //compiling fetch url
-    const url = `${googlePlacesApiURL}/nearbysearch/json?location=${lat},${long}&radius=${radius}&type=restaurant${keyword}&key=${googlePlacesApiKey}`;
-    console.log(url)
+    const url = `${googlePlacesApiURL}/nearbysearch/json?location=${lat},${long}&radius=${radius}&type=restaurant&keyword=${keyword}&key=${googlePlacesApiKey}`;
     fetch(url).then((response) => {
         return response.json()
     }).then(data => {
-        fillArr=data.results
-        console.log(data);
+        fillArr=processRestaurantSearch(data.results,googlePlacesApiKey)
         if(data.next_page_token && fillArr.length<minLength) {
             getNextPage(data.next_page_token,fillArr,minLength,res) 
         }else{
-            fillArr= processRestaurantSearch(fillArr,googlePlacesApiKey).sort((a,b)=> a.rating-b.rating);
+            fillArr= fillArr.sort((a,b)=> a.rating-b.rating);
             res.status(200).json({results:fillArr});
         } 
     }).catch(err => { 
@@ -38,16 +37,19 @@ function getPlaces(req, res) {
 }
 
 function getNextPage(nextPageToken,fillArr,minLength,res) {
+    
+    counter= counter+1;
+    console.log(counter);
     const url = `${googlePlacesApiURL}/nearbysearch/json?pagetoken=${nextPageToken}&key=${googlePlacesApiKey}`;
     setTimeout(()=>{
         fetch(url).then((response) => {
             return response.json()
         }).then(data=>{
-            fillArr=fillArr.concat(data.results);
+            fillArr=fillArr.concat(processRestaurantSearch(data.results,googlePlacesApiKey));
             if(data.next_page_token && fillArr.length<minLength) {
                 getNextPage(data.next_page_token,fillArr,minLength,res)
             }else{
-                fillArr= processRestaurantSearch(fillArr,googlePlacesApiKey).sort((a,b)=> a.rating-b.rating);
+                fillArr= fillArr.sort((a,b)=> a.rating-b.rating);
                 res.status(200).json({results:fillArr});
             } 
         }).catch(err=> console.log(err))
--- BE/lib/processRestaurantSearch.js
+++ BE/lib/processRestaurantSearch.js
@@ -1,7 +1,9 @@
 function processRestaurantSearch(results, APIkey){
 
-  let outputArray = results.map(function(restaurant) {
+  let outputArray = results.reduce(function(acc,restaurant) {
     let outputRestaurantObject = {};
+    if(+(restaurant.rating)<3.5){
+    
 
     if (restaurant.hasOwnProperty("photos")){
       let photoRef = restaurant.photos[0].photo_reference;
@@ -19,8 +21,44 @@ function processRestaurantSearch(results, APIkey){
     outputRestaurantObject.place_id=restaurant.place_id;
     outputRestaurantObject.rating = restaurant.rating;
 
-    return outputRestaurantObject;
-  });
+    acc.push(outputRestaurantObject)
+  }
+    return acc;
+  },[]);
+
+  return outputArray;
+
+}
+
+module.exports = processRestaurantSearch;
+
+function processRestaurantSearch(results, APIkey){
+
+  let outputArray = results.reduce(function(acc,restaurant) {
+    let outputRestaurantObject = {};
+    if(+(restaurant.rating)<3.5){
+    
+
+    if (restaurant.hasOwnProperty("photos")){
+      let photoRef = restaurant.photos[0].photo_reference;
+      //outputRestaurantObject.photoURL = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${photoRef}&maxheight=400&key=${APIkey}`;
+      outputRestaurantObject.photoURL = `https://images.pexels.com/photos/6267/menu-restaurant-vintage-table.jpg?w=800&h=600&dpr=3&auto=compress&cs=tinysrgb`;
+    } else {
+      outputRestaurantObject.photoURL = "placeholder-image";
+    }
+
+    //TODO add actual placeholder image path
+
+    outputRestaurantObject.location = restaurant.geometry.location;
+    outputRestaurantObject.name = restaurant.name;
+
+    outputRestaurantObject.place_id=restaurant.place_id;
+    outputRestaurantObject.rating = restaurant.rating;
+
+    acc.push(outputRestaurantObject)
+  }
+    return acc;
+  },[]);
 
   return outputArray;
 
